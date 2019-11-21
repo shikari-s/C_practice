e38e8e351daa02c00f62a27304560d05eb5bf112
shikari-s <41614002+shikari-s@users.noreply.github.com>
2019-11-21 09:22:01 +0900

    Create lec9.c

---
+++ lec9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+double f(double);
+
+int main(void){
+        double s = 0.0;
+        double a = 0.0;
+        double b = 1.1750;
+        int n = 10;
+        double h = (b-a)/(double)n;
+        int k = 0;
+        s = (f(a)+f(b)/2.0)*h;
+        for(k = 1;k < n;k++){
+                s += f(a+k*h)*h;
+        }
+        printf("%d, %f\n",n,s);
+}
+
+double f(double x){
+        return x*x;
+}
