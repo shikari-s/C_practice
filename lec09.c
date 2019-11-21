47a0ed3572ad01863261f56d9020cb4257c9b24b
shikari-s <41614002+shikari-s@users.noreply.github.com>
2019-11-21 09:22:55 +0900

    Update lec09.c

--- lec09.c
+++ lec09.c
@@ -4,7 +4,7 @@ double f(double);
 int main(void){
         double s = 0.0;
         double a = 0.0;
-        double b = 1.1750;
+        double b = 1.wxyz;
         int n = 10;
         double h = (b-a)/(double)n;
         int k = 0;
